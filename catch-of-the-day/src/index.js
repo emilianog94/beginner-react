93019ca1ca8df3b6327ab9f4a3dd28610fe8cf7f
Emi <emiliano@gioia.com.ar>
2022-02-14 20:49:30 -0300

    css

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import StorePicker from './components/StorePicker';
+import './css/style.css';
+
 
 render(
     <StorePicker/>,
     document.querySelector('#main')
 );
-
-
-console.log("heu");
