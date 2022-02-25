6f2db940ca4701e9fcab2ed22399378c8f695471
Emi <emiliano@gioia.com.ar>
2022-02-24 23:49:37 -0300

    Renderizo order state

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,10 +1,43 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component{
+
+    renderItems = (key) => {
+        const fish = this.props.fishes[key];
+        if(fish.status == "available") {
+            return(
+                <li key={key}>{fish.name} - {this.props.order[key]}x {formatPrice(fish.price)} </li>
+            )
+        } else{
+            return(
+                <li key={key}>Sorry! the fish you previously picked ({fish.name}) is unavailable right now!</li>
+            )
+        }
+    }
+
+    renderTotal = () => {
+        const fishes = Object.keys(this.props.order);
+        const precio = fishes.reduce( (total, key) => {
+            const fish = this.props.fishes[key];
+            const price = fish.price;
+            const quantity =  this.props.order[key]
+            fish.status === "available" ? total = total + ( quantity * price) : total
+            return total;
+        },0);
+        return formatPrice(precio);
+    }
+
     render(){
         return(
             <div className="order">
-                Soy order
+                <ul>
+                    {Object.keys(this.props.order).map(this.renderItems)}
+                </ul>
+
+                <p className="total">
+                    Total: {this.renderTotal()}
+                </p>
             </div>
         )
     }
